22093600a9833b99386dd33ef144ec482ce665d0
prateek211205361719 <prateek@ceptes.com>
2017-08-31 23:36:34 +0530

    creating safe key

--- server/config/key.js
+++ server/config/key.js
@@ -1,6 +1,8 @@
-module.exports = {
-    googleAppId:"193331896853-s16u7s5mr9vkttct8vu1n9p2mnof8gnv.apps.googleusercontent.com",
-    googleSecret:"O2EO_pS9gVACHZKI7A1o1Gxx",
-    mongoDbURI: "mongodb://prateek211205:sanu211205@ds163053.mlab.com:63053/feedback_dev"
 
-};
+
+if(process.env.NODE_ENV === 'production'){
+   module.exports = require('./prod');
+    //we are in production
+}else{
+    module.exports = require('./dev');
+}
---
+++ server/config/prod.js
@@ -0,0 +1,8 @@
+
+
+module.exports = {
+    googleAppId: process.env.GOOGLE_APP_ID,
+    googleSecret: process.env.GOOGLE_SECRET,
+    mongoDbURI: process.env.MONGO_DB_URI
+
+};
